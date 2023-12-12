2d63e6f4bfac0e413ca973375f144dc2849578d1
Arnidh <145589189+Arnidh@users.noreply.github.com>
2023-12-12 23:03:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+
+const MyButton = document.getElementById('Inquire-btn');
+MyButton.addEventListener('click', function() {
+    document.getElementById('btn-txt').innerText = "Confirmation email with details of booking sent to the entered email id..!"
+})
+
+const MyButton2= document.getElementById('sub-btn');
+MyButton2.addEventListener('click', function() {
+    document.getElementById('sub-txt').innerText = "U will now recieve regular updates through the entered email id..!"
+})
+
+document.getElementById("about-btn").onclick = function () {
+    location.href = "About.html";
+};
+
+document.getElementById("contact-btn").onclick = function () {
+    location.href = "Contact.html";
+};
+
+document.getElementById("mumbai").onclick = function () {
+    location.href = "Mumbai.html";
+};
+
+document.getElementById("goa").onclick = function () {
+    location.href = "Goa.html";
+};
+
+document.getElementById("blore").onclick = function () {
+    location.href = "Bangalore.html";
+};
+
+document.getElementById("chennai").onclick = function () {
+    location.href = "Chennai.html";
+};
+
+document.getElementById("kolkata").onclick = function () {
+    location.href = "Kolkata.html";
+};
+
+document.getElementById("jaipur").onclick = function () {
+    location.href = "jaipur.html";
+};
+
+document.getElementById("japan").onclick = function () {
+    location.href = "Japan.html";
+};
+
+document.getElementById("dubai").onclick = function () {
+    location.href = "Dubai.html";
+};
+
+document.getElementById("paris").onclick = function () {
+    location.href = "Paris.html";
+};
+
+document.getElementById("seoul").onclick = function () {
+    location.href = "Seoul.html";
+};
+
+document.getElementById("maldives").onclick = function () {
+    location.href = "Maldives.html";
+};
+
+document.getElementById("singapore").onclick = function () {
+    location.href = "Singapore.html";
+};
